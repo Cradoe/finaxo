cc41336396b6815edf334e63a6bdff7f4ec2a3be
Cradoe <me@ismailobadimu.com>
2021-06-16 08:32:13 +0100

    Initialised PFA module

---
+++ account/pfa/dashboard.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include "includes/header-resources.php"; ?>
+</head>
+
+<body>
+  <!-- tap on top starts-->
+  <div class="tap-top"><i data-feather="chevrons-up"></i></div>
+  <!-- tap on tap ends-->
+  <!-- page-wrapper Start-->
+  <div class="page-wrapper compact-wrapper" id="pageWrapper">
+    <!-- Page Header Start-->
+    <?php include "includes/header.php"; ?>
+    <!-- Page Header Ends                              -->
+    <!-- Page Body Start-->
+    <div class="page-body-wrapper">
+      <!-- Page Sidebar Start-->
+      <?php include "includes/sidebar.php"; ?>
+
+      <!-- Page Sidebar Ends-->
+      <div class="page-body">
+        <div class="container-fluid">
+          <div class="page-title">
+            <div class="row">
+              <div class="col-6">
+                <h3>
+                  Flexidink</h3>
+              </div>
+              <div class="col-6">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="index.html"><i data-feather="home"></i></a></li>
+                  <li class="breadcrumb-item">Dashboard</li>
+                  <li class="breadcrumb-item active">Flexidink</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Container-fluid starts-->
+        <div class="container-fluid">
+          <div class="row size-column">
+            <div class="col-xl-3 risk-col xl-100 box-col-12">
+              <div class="card total-users">
+                <div class="card-header card-no-border">
+                  <h5>At a Glance</h5>
+
+                </div>
+                <div class="card-body pt-0">
+                  <div class="apex-chart-container goal-status text-center row">
+                    <div class="rate-card col-xl-12">
+                      <div class="goal-chart">
+                        <div id="riskfactorchart"></div>
+                      </div>
+                      <div class="goal-end-point">
+                        <ul>
+                          <li class="mt-0 pt-0">
+                            <h6 class="font-primary">As From</h6>
+                            <h6 class="f-w-400">24 March 2021</h6>
+                          </li>
+                          <li>
+                            <h6 class="mb-2 f-w-400">Total Staffs</h6>
+                            <h5 class="mb-0">7977</h5>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <ul class="col-xl-12">
+                      <li>
+                        <div class="goal-detail">
+                          <h6> <span class="font-primary">Pension Archive : </span>&#8358;91,000.000</h6>
+                          <div class="progress sm-progress-bar progress-animate">
+                            <div class="progress-gradient-primary" role="progressbar" style="width: 60%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </div>
+                        <div class="goal-detail mb-0">
+                          <h6><span class="font-primary">Duration: </span>9 Month</h6>
+                          <div class="progress sm-progress-bar progress-animate">
+                            <div class="progress-gradient-primary" role="progressbar" style="width: 50%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="btn-download btn btn-gradient f-w-500">Download details</div>
+                      </li>
+                    </ul>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-9 xl-100 box-col-12">
+              <div class="row">
+                <div class="col-xl-12">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="best-seller-table responsive-tbl">
+                        <div class="item">
+                          <div class="table-responsive product-list">
+                            <table class="table table-bordernone">
+                              <thead>
+                                <tr>
+                                  <th class="f-22">
+                                    Recent Transactions</th>
+                                  <th>Ref</th>
+                                  <th>Date</th>
+                                  <th>Amount</th>
+                                  <th class="text-end">Status</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <div class="d-inline-block align-middle"><img class="img-40 m-r-15 rounded-circle align-top" src="../assets/images/avtar/7.jpg" alt="">
+                                      <div class="status-circle bg-primary"></div>
+                                      <div class="d-inline-block"><span>John keter</span>
+                                        <p class="font-roboto">Human Resources</p>
+                                      </div>
+                                    </div>
+                                  </td>
+                                  <td>#ijij8menjknj</td>
+                                  <td>06 August</td>
+                                  <td> <span class="label">&#8358;5,08,652</span></td>
+                                  <td class="text-end"><i class="fa fa-check-circle"></i>Successful</td>
+                                </tr>
+                                <tr>
+                                  <td>
+                                    <div class="d-inline-block align-middle"><img class="img-40 m-r-15 rounded-circle align-top" src="../assets/images/avtar/7.jpg" alt="">
+                                      <div class="status-circle bg-primary"></div>
+                                      <div class="d-inline-block"><span>John keter</span>
+                                        <p class="font-roboto">Human Resources</p>
+                                      </div>
+                                    </div>
+                                  </td>
+                                  <td>#ijij8menjknj</td>
+                                  <td>06 August</td>
+                                  <td> <span class="label">&#8358;5,08,652</span></td>
+                                  <td class="text-end"><i class="fa fa-check-circle"></i>Successful</td>
+                                </tr>
+                                <tr>
+                                  <td>
+                                    <div class="d-inline-block align-middle"><img class="img-40 m-r-15 rounded-circle align-top" src="../assets/images/avtar/7.jpg" alt="">
+                                      <div class="status-circle bg-primary"></div>
+                                      <div class="d-inline-block"><span>John keter</span>
+                                        <p class="font-roboto">Human Resources</p>
+                                      </div>
+                                    </div>
+                                  </td>
+                                  <td>#ijij8menjknj</td>
+                                  <td>06 August</td>
+                                  <td> <span class="label">&#8358;5,08,652</span></td>
+                                  <td class="text-end"><i class="fa fa-check-circle"></i>Successful</td>
+                                </tr>
+                                <tr>
+                                  <td>
+                                    <div class="d-inline-block align-middle"><img class="img-40 m-r-15 rounded-circle align-top" src="../assets/images/avtar/7.jpg" alt="">
+                                      <div class="status-circle bg-primary"></div>
+                                      <div class="d-inline-block"><span>John keter</span>
+                                        <p class="font-roboto">Human Resources</p>
+                                      </div>
+                                    </div>
+                                  </td>
+                                  <td>#ijij8menjknj</td>
+                                  <td>06 August</td>
+                                  <td> <span class="label">&#8358;5,08,652</span></td>
+                                  <td class="text-end"><i class="fa fa-check-circle"></i>Successful</td>
+                                </tr>
+
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Container-fluid Ends-->
+      </div>
+      <!-- footer start-->
+
+      <?php include "includes/footer.php"; ?>
+
+
+    </div>
+  </div>
+  <?php include "includes/footer-resources.php"; ?>
+
+  <script src="../assets/js/chart/apex-chart/apex-chart.js"></script>
+
+  <script>
+    $(function() {
+
+
+      var options = {
+        series: [100],
+        chart: {
+          height: 350,
+          type: 'radialBar',
+          offsetY: -10,
+        },
+
+        plotOptions: {
+          radialBar: {
+            startAngle: -135,
+            endAngle: 135,
+            inverseOrder: true,
+            hollow: {
+              margin: 5,
+              size: '60%',
+              image: '../assets/images/dashboard-2/radial-image.png',
+              imageWidth: 140,
+              imageHeight: 140,
+              imageClipped: false,
+            },
+            track: {
+              opacity: 0.4,
+              colors: CubaAdminConfig.primary
+            },
+            dataLabels: {
+              enabled: false,
+              enabledOnSeries: undefined,
+              formatter: function(val, opts) {
+                return val + "%"
+              },
+              textAnchor: 'middle',
+              distributed: false,
+              offsetX: 0,
+              offsetY: 0,
+
+              style: {
+                fontSize: '14px',
+                fontFamily: 'Helvetica, Arial, sans-serif',
+                fill: ['#2b2b2b'],
+
+              },
+            },
+          }
+        },
+
+        fill: {
+          type: 'gradient',
+          gradient: {
+            shade: 'light',
+            shadeIntensity: 0.15,
+            inverseColors: false,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0, 100],
+            gradientToColors: ['#a927f9'],
+            type: 'horizontal'
+          },
+        },
+        stroke: {
+          dashArray: 15,
+          strokecolor: ['#ffffff']
+        },
+
+        labels: ['Performance'],
+        colors: [CubaAdminConfig.primary],
+      };
+
+      var chart4 = new ApexCharts(document.querySelector("#riskfactorchart"),
+        options
+      );
+      chart4.render();
+    })
+  </script>
+</body>
+
+</html>
---
+++ account/pfa/departments.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "includes/header-resources.php"; ?>
+
+    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/datatables.css">
+    <style>
+        .img-avatar {
+            height: 50px;
+            width: 50px;
+            border-radius: 50px
+        }
+    </style>
+</head>
+
+<body>
+    <!-- tap on top starts-->
+    <div class="tap-top"><i data-feather="chevrons-up"></i></div>
+    <!-- tap on tap ends-->
+    <!-- page-wrapper Start-->
+    <div class="page-wrapper compact-wrapper" id="pageWrapper">
+        <!-- Page Header Start-->
+        <?php include "includes/header.php"; ?>
+        <!-- Page Header Ends                              -->
+        <!-- Page Body Start-->
+        <div class="page-body-wrapper">
+            <!-- Page Sidebar Start-->
+            <?php include "includes/sidebar.php"; ?>
+            <!-- Page Sidebar Ends-->
+            <div class="page-body">
+                <div class="container-fluid">
+                    <div class="page-title">
+                        <div class="row">
+                            <div class="col-6">
+                                <h3>Departments</h3>
+                            </div>
+                            <div class="col-6">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html"> <i data-feather="home"></i></a></li>
+                                    <li class="breadcrumb-item">Pages</li>
+                                    <li class="breadcrumb-item active">Departments</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Container-fluid starts-->
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h5>Departments</h5><span>See record of funds remited by your employer.</span>
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="display" id="datatable">
+                                            <thead>
+                                                <tr>
+                                                    <th>S/N</th>
+                                                    <th>Department</th>
+                                                    <th>Date Registered</th>
+                                                    <th>Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>2011/04/25</td>
+                                                    <td>
+                                                        <a href="staffs?department=1" class="btn btn-xs btn-primary" target="_blank">
+                                                            View Staffs
+                                                        </a>
+
+                                                        <a href="#" class="btn btn-danger confirm btn-xs">
+                                                            Delete
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>2011/04/25</td>
+                                                    <td>
+                                                        <a href="staffs?department=1" class="btn btn-xs btn-primary" target="_blank">
+                                                            View Staffs
+                                                        </a>
+
+                                                        <a href="#" class="btn btn-danger confirm btn-xs">
+                                                            Delete
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>2011/04/25</td>
+                                                    <td>
+                                                        <a href="staffs?department=1" class="btn btn-xs btn-primary" target="_blank">
+                                                            View Staffs
+                                                        </a>
+
+                                                        <a href="#" class="btn btn-danger confirm btn-xs">
+                                                            Delete
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>2011/04/25</td>
+                                                    <td>
+                                                        <a href="staffs?department=1" class="btn btn-xs btn-primary" target="_blank">
+                                                            View Staffs
+                                                        </a>
+
+                                                        <a href="#" class="btn btn-danger confirm btn-xs">
+                                                            Delete
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Container-fluid Ends-->
+            </div>
+            <!-- footer start-->
+            <?php include "includes/footer.php"; ?>
+        </div>
+    </div>
+    <?php include "includes/footer-resources.php"; ?>
+
+    <script src="../assets/js/datatable/datatables/jquery.dataTables.min.js"></script>
+    <script>
+        $('#datatable').DataTable();
+    </script>
+    <!-- Plugin used-->
+</body>
+
+</html>
